77c67caa7fc29192051207b4d7f1a7a379d9e16a
Manav Misra <manavm@visionwebsoft.com>
2020-12-26 12:34:51 -0600

    Integrate postcss nesting

--- craco.config.js
+++ craco.config.js
@@ -4,6 +4,7 @@ module.exports = {
       plugins: [
         require("postcss-import"),
         require("tailwindcss"),
+        require("postcss-nesting"),
         require("autoprefixer"),
       ],
     },
