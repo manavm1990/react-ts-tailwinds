12675d41c0cf9e444b8e8814d6a7b38a94f896a5
Manav Misra <manavm@visionwebsoft.com>
2020-12-26 19:49:30 -0600

    :lipstick: Add initial styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import "./App.css";
 import logo from "./logo.svg";
 
 const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+  <div className="text-center">
+    <header className="bg-primary flex flex-col align-center justify-center text-10+2 min-h-screen text-white">
+      <img
+        src={logo}
+        className="motion-safe:animate-spin-slow h-40-vmin pointer-events-none"
+        alt="logo"
+      />
       <p>
         Edit <code>src/App.tsx</code> and save to reload.
       </p>
       <a
-        className="App-link"
         href="https://reactjs.org"
         target="_blank"
         rel="noopener noreferrer"
+        className="text-primary"
       >
         Learn React
       </a>
